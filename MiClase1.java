84b9fa1019eecf4af8ad9715a1ef18770ebbd4f1
josepdurba <josepdurbagavila@gmail.com>
2025-11-28 17:39:33 +0100

    Añadido hola mundo

--- MiClase1.java
+++ MiClase1.java
@@ -1,6 +1,7 @@
 public class MiClase1{
 
     public static void main(String args[]) {
+        System.out.println("¡Hola, mundo!");
         for(int i = 0; i <= 10; i++) {
             System.out.println("12 * "+ i + " = " + 12 * i + "\n");
         }
