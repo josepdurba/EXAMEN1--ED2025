ce1037f340083b4d081090681c47cf70e66d54dc
josepdurba <josepdurbagavila@gmail.com>
2025-11-28 17:48:36 +0100

    Añadido ¿como estas?

--- MiClase1.java
+++ MiClase1.java
@@ -4,5 +4,6 @@ public class MiClase1{
         for(int i = 0; i <= 10; i++) {
             System.out.println("12 * "+ i + " = " + 12 * i + "\n");
         }
+        System.out.println("¿Como estas?");
     }
 }
